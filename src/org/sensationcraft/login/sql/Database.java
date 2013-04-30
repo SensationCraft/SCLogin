d2bad9ef6137093548b8f63a6bd0ed7474530367
Mark Hendriks <datfireblast709@gmail.com>
2013-04-30 23:09:12 +0200

    Added some structure for SQL management

---
+++ src/org/sensationcraft/login/sql/Database.java
@@ -0,0 +1,54 @@
+package org.sensationcraft.login.sql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author s129977
+ */
+public abstract class Database 
+{
+    
+    protected Connection con;
+    
+    private final Logger log;
+    
+    private final String prefix;
+    
+    protected Database(String prefix, Logger log)
+    {
+        this.log = log;
+        this.prefix = prefix;
+    }
+    
+    protected abstract boolean initialize();
+    
+    protected abstract Connection connect();
+    
+    protected boolean isReady()
+    {
+        if(this.con == null)
+        {
+            log("Tried to execute a query or to prepare a statement while the connection was not ready.");
+            return false;
+        }
+        return true;
+    }
+    
+    protected abstract boolean checkTable(String name);
+    
+    protected abstract boolean createTable();
+    
+    protected abstract ResultSet query(String query);
+    
+    protected abstract PreparedStatement prepare(String query);
+    
+    protected void log(String msg, Object...o)
+    {
+        log.log(Level.SEVERE, prefix+msg, o);
+    }
+}
---
+++ src/org/sensationcraft/login/sql/SQLite.java
@@ -0,0 +1,89 @@
+package org.sensationcraft.login.sql;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author s129977
+ */
+public class SQLite extends Database
+{
+        
+    public SQLite(String prefix, Logger log, File dbfile)
+    {
+        super(prefix, log);
+    }
+    
+    @Override
+    public boolean initialize()
+    {
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            return true;
+        }
+        catch(ClassNotFoundException ex)
+        {
+            log("SQLite library not found!");
+            return false;
+        }
+    }
+    
+    @Override
+    public Connection connect()
+    {
+        if(initialize())
+        {
+            try
+            {
+                this.con = DriverManager.getConnection("");
+            }
+            catch (SQLException ex)
+            {
+                log("");
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected boolean checkTable(String name)
+    {
+        if(!isReady()) return false;
+        // Check
+        return true;
+    }
+
+    @Override
+    protected boolean createTable()
+    {
+        if(!isReady()) return false;
+        return true;
+    }
+
+    @Override
+    protected ResultSet query(String query)
+    {
+        if(!isReady()) return null;
+        
+        ResultSet result = null;
+        
+        return result;
+    }
+
+    @Override
+    protected PreparedStatement prepare(String query)
+    {
+        if(!isReady()) return null;
+        PreparedStatement stmt = null;
+        return stmt;
+    }
+
+}
